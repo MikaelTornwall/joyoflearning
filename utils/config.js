37dc076a3b54395cff8606f5573a7078cebc02ad
Mikael Törnwall <mikael.tornwall@aalto.fi>
2019-06-19 14:54:10 +0300

    add env variables for test and production db

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('dotenv').config()
 
 const mongoUrl = process.env.MONGODB_URI
 const port = process.env.PORT
+
+if (process.env.NODE_ENV === 'production') {
+  MONGODB_URI = process.env.PRODUCTION_MONGODB_URI
+}
+
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI
+}
 
 module.exports = { mongoUrl, port }
