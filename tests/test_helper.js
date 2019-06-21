b7c950d80908cc4aa70c2dd92ace5a61af699ddd
Mikael Törnwall <mikael.tornwall@aalto.fi>
2019-06-21 15:40:07 +0300

    more tests

---
+++ tests/test_helper.js
@@ -0,0 +1,38 @@
+const User =                require('../models/user')
+
+const initialUsers = [
+  {
+    firstname: 'Ensimmäinen',
+    lastname: 'Testikäyttäjä',
+    username: 'Ensimmäinen',
+    email: 'ensimmäinen@email.com',
+    password: 'salasana',
+    organization: 'TestiFirma',
+    date: new Date()
+  },
+  {
+    firstname: 'Toinen',
+    lastname: 'Testikäyttäjä',
+    username: 'Toinen',
+    email: 'toinen@email.com',
+    password: 'salasana',
+    organization: 'ToinenTestiFirma',
+    date: new Date()
+  },
+  {
+    firstname: 'Kolmas',
+    lastname: 'Testaaja',
+    username: 'Kolmas',
+    email: 'kolmas@email.com',
+    password: 'salasana',
+    organization: 'KolmasTestiYritys',
+    date: new Date()
+  }
+]
+
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(user => user.toJSON())
+}
+
+module.exports = { initialUsers, usersInDb }
--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -1,14 +1,51 @@
-const mongoose =    require('mongoose')
-const supertest =   require('supertest')
-const app =         require('../app')
+const mongoose =            require('mongoose')
+const supertest =           require('supertest')
+const app =                 require('../app')
+const helper =              require('./test_helper')
+const User =                require('../models/user')
+const api =                 supertest(app)
 
-const api = supertest(app)
+beforeEach(async () => {
+  await User.remove({})
+  console.log('User database cleared')
+
+  const userObjects = helper.initialUsers
+    .map(user => new User(user))
+
+  const promiseArray = userObjects.map(user => user.save())
+  await Promise.all(promiseArray)
+  console.log('User database initialized')
+})
+
+test('all users are returned', async () => {
+  const res = await helper.usersInDb()
+
+  expect(res.length).toBe(helper.initialUsers.length)
+})
+
+test('a specific note is within the returned notes', async () => {
+  const res = await helper.usersInDb()
+
+  const usernames = res.map(data => data.username)
+
+  expect(usernames).toContain('Ensimmäinen')
+})
 
 test('users are returned as json', async () => {
   await api
     .get('/api/users')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('a specific user can be returned', async () => {
+  const res = await helper.usersInDb()
+
+  const firstUser = res[0]
+
+  const returnedUser = await User.findById(firstUser.id)
+
+  expect(returnedUser.username).toBe(firstUser.username)
 })
 
 afterAll(() => {
